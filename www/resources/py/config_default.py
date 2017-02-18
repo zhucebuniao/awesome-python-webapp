04c1760b7fd9f6e91c4d15990e9c316cde4fd2bd
QXQ <852590961@qq.com>
2017-02-18 19:17:11 +0800

    前端框架

---
+++ www/resources/py/config_default.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+'''
+默认配置
+'''
+
+configs = {
+    'debug' : True,
+    'db' : {
+        'host' : '127.0.0.1',
+        'port' : 3306,
+        'user' : 'root',
+        'password' : 'root',
+        'db' : 'test'
+    },
+    'session' : {
+        'secret' : 'test'
+    }
+}
---
+++ www/resources/py/config_override.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+'''
+override conf
+'''
+
+configs = {
+    'db' : {
+        'host' : '127.0.0.1'
+    }
+}
